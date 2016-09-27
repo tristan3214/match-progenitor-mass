2f673fe3f4625d2f5997cbd5378218aac0509c67
Tristan Hillis <tristan3214@live.com>
2016-09-27 14:12:14 -0700

    tjh

--- MatchParam.py
+++ MatchParam.py
@@ -570,7 +570,7 @@ class MatchParam(object):
 
         nextFile = ""
         if size > 0:
-            numbers = [int(files[i].split("_")[1].split(".")[0]) for i in xrange(len(files)
+            numbers = [int(files[i].split("_")[1].split(".")[0]) for i in xrange(len(files))]
             print(numbers)
             numbers = sorted(numbers, key=int)[-1]
 
