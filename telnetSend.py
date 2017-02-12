4827bd81387af4fe3246de90407a6c48b4fe8e7d
Tristan Hillis <tristan3214@live.com>
2017-02-11 20:02:23 -0800

    tjh

--- telnetSend.py
+++ telnetSend.py
@@ -6,8 +6,8 @@ import telnetlib
 import threading
 import time
 
-#HOST = "10.155.88.139"
-HOST = "10.155.88.135" # astrolab18
+HOST = "10.155.88.139"
+#HOST = "10.155.88.135" # astrolab18
 PORT = 42424
 
 def main():
