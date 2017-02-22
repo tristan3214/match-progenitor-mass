3dd0ca45006bf09e6725716c0fac9d51a1449e2c
Tristan Hillis <tristan3214@live.com>
2017-02-22 14:18:17 -0800

    tjh

--- ServerMATCH.py
+++ ServerMATCH.py
@@ -1,4 +1,4 @@
-#!/astro/app6/anaconda/bin/python2
+#!/astro/apps6/anaconda/bin/python2
 from __future__ import print_function
 from __future__ import division
 from __future__ import absolute_import
