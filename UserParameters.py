2e25673e1df419e80ab580a47719b5bb252915ae
Tristan Hillis <tristan3214@live.com>
2017-04-28 14:25:54 -0700

    tjh

--- UserParameters.py
+++ UserParameters.py
@@ -11,9 +11,9 @@ import os
 
 ####
 #CORE_COUNT = multiprocessing.cpu_count() # How many threads will run commands (runningCommands = CORE_COUNT-1)
-CORE_COUNT = 0
+CORE_COUNT = 8
 MAX_CONDOR_SIZE = 3000 # This will be the max size of the queued jobs in condor
-CONDOR_ON = True # Changes this to False if you don't want to use condor
+CONDOR_ON = False # Changes this to False if you don't want to use condor
 #USER_ID = getpass.getuser() # This will keep track of the user id
 PORT_NUMBER = 42424 # Change this if somebody else is using the same port
 HOST_IP_ADDRESS = "10.155.88.139" # Change to IP address that your server is running on.  Currently set to Eagle.
