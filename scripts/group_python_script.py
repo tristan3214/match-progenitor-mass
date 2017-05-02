9b68a45c539cb55b371405780d54c2c6cbf19e9e
Tristan Hillis <tristan3214@live.com>
2017-05-02 12:51:23 -0700

    tjh

--- scripts/group_python_script.py
+++ scripts/group_python_script.py
@@ -31,7 +31,7 @@ def runProcessDav(path, commands):
     baseName = "_".join(davFit.fit.split("_")[:-2])
     photometry = davFit.phot
     param_file = davFit.parameter
-    #ProcessDAv(path, baseName, photometry, param_file, bins=2)
+    ProcessDAv(path, baseName, photometry, param_file, bins=2)
     
     return None
 
